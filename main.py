65be0a45a007bd0c6c715ac6d19a5fa72f3b37b8
Timon2411 <102752770+Timon2411@users.noreply.github.com>
2022-04-16 13:58:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ def start_message(message):
     keyboard.add("/wish10")
     keyboard.add("/hatch")
     keyboard.add("/wepon")
-    keyboard.add("/p")
+    keyboard.add("/pдфн")
     bot.send_message(message.from_user.id, "Здравствуй! Что собираешься делать", reply_markup=keyboard)
 
 @bot.message_handler(commands=['привет'])
 def handle_command(message):
   bot.send_message(message.chat.id, 'Привет,что хотите сейчас делать?')
 
-@bot.message_handler(commands=['played'])
+@bot.message_handler(commands=['play'])
 def handle_command(message):
   bot.send_message(message.chat.id, '@Botiara_bard_bard22_bot')
 
